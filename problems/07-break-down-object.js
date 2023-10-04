28c9091d9bc02b9c9c07aaca645e07aa9080624a
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2023-10-04 07:49:28 -0500

    first

--- problems/07-break-down-object.js
+++ problems/07-break-down-object.js
@@ -15,7 +15,9 @@ breakDownObj(object2); // => [ 'location', 'borough', 'NY', 'Brooklyn' ]
 ***********************************************************************/
 
 function breakDownObj(obj) {
-  // Your code here
+  const keys = Object.keys(obj);
+  const values = Object.keys(obj);
+  return [...keys, ...values];
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
